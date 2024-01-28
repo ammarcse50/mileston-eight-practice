1f15c495d12e419bdc90ca528b3084cb4b6b290e
ammmar <ammaraslam7164@gmail.com>
2024-01-28 23:14:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+  
+  return <>
+   
+  <Navbar> </Navbar>
+  
+   <h2>Vite+react=love</h2>
+  
+  
+  </>;
+}
+
+export default App;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <ul>
+
+             {}
+
+            </ul>
+            
+        </div>
+    );
+};
+
+Navbar.prototype={
+
+
+
+}
+
+export default Navbar;
